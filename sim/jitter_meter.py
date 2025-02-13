326e7ef46c4e9ec1c98f5af66345432732fc9983
Victor Gaydov <victor@enise.org>
2025-02-14 06:24:48 +0900

    Update sim

--- sim/jitter_meter.py
+++ sim/jitter_meter.py
@@ -1,74 +1,78 @@
 #! /usr/bin/env python3
+# -*- coding: utf-8; tab-width: 2 -*-
 
 from .mov_stats import *
 from dataclasses import dataclass
+import numpy as np
 
 @dataclass
 class JitterMeterConfig:
-    jitter_window: int = 50000
-    envelope_smoothing_window_len: int = 10
-    envelope_resistance_exponent: float = 6.0
-    envelope_resistance_coeff: float = 0.1
-    peak_quantile_window: int = 10000
-    peak_quantile_coeff: float = 0.92
+  jitter_window: int = 50000
+  envelope_smoothing_window_len: int = 10
+  envelope_resistance_exponent: float = 6.0
+  envelope_resistance_coeff: float = 0.1
+  peak_quantile_window: int = 10000
+  peak_quantile_coeff: float = 0.92
 
 @dataclass
 class JitterMetrics:
-    mean_jitter: int = 0
-    peak_jitter: int = 0
-    curr_jitter: int = 0
-    curr_envelope: int = 0
+  mean_jitter: float = 0
+  peak_jitter: float = 0
+  curr_jitter: float = 0
+  curr_envelope: float = 0
 
 class JitterMeter:
-    """
-    >>> meter = JitterMeter()
-    >>> meter.update_jitter(1000)
-    >>> meter.update_jitter(1500)
-    >>> meter.update_jitter(1000)
-    >>> meter.metrics()
-    JitterMetrics(mean_jitter=1166, peak_jitter=1500, curr_jitter=1000, curr_envelope=1500)
-    """
+  """
+  >>> meter = JitterMeter()
+  >>> meter.update_jitter(1000)
+  >>> meter.update_jitter(1500)
+  >>> meter.update_jitter(1000)
+  >>> meter.metrics()
+  JitterMetrics(mean_jitter=1167, peak_jitter=1500, curr_jitter=1000, curr_envelope=1500)
+  """
 
-    def __init__(self, config=None):
-        if not config:
-            config = JitterMeterConfig()
-        self._config = config
-        self._metrics = JitterMetrics()
-        self._jitter_window = MovAvgStd(config.jitter_window)
-        self._smooth_jitter_window = MovMinMax(config.envelope_smoothing_window_len)
-        self._envelope_window = MovQuantile(config.peak_quantile_window,
-                                            config.peak_quantile_coeff)
-        self._peak_window = MovMinMax(config.jitter_window)
-        self._capacitor_charge = 0
-        self._capacitor_discharge_resistance = 0
-        self._capacitor_discharge_iteration = 0
+  def __init__(self, config=None):
+    if not config:
+        config = JitterMeterConfig()
+    self._config = config
+    self._metrics = JitterMetrics()
+    self._jitter_window = MovAvgStd(config.jitter_window)
+    self._smooth_jitter_window = MovMinMax(config.envelope_smoothing_window_len)
+    self._envelope_window = MovQuantile(config.peak_quantile_window,
+                                        config.peak_quantile_coeff)
+    self._peak_window = MovMinMax(config.jitter_window)
+    self._capacitor_charge = 0
+    self._capacitor_discharge_resistance = 0
+    self._capacitor_discharge_iteration = 0
 
-    def metrics(self):
-        return self._metrics
+  def metrics(self):
+    return self._metrics
 
-    def update_jitter(self, jitter: int):
-        self._jitter_window.add(jitter)
-        self._smooth_jitter_window.add(jitter)
-        jitter_envelope = self._update_envelope(
-            self._smooth_jitter_window.mov_max(), self._jitter_window.mov_avg())
-        self._envelope_window.add(jitter_envelope)
-        self._peak_window.add(self._envelope_window.mov_quantile())
-        self._metrics.mean_jitter = int(self._jitter_window.mov_avg())
-        self._metrics.peak_jitter = int(self._peak_window.mov_max())
-        self._metrics.curr_jitter = int(jitter)
-        self._metrics.curr_envelope = int(jitter_envelope)
+  def update_jitter(self, jitter):
+    self._jitter_window.add(jitter)
+    self._smooth_jitter_window.add(jitter)
 
-    def _update_envelope(self, cur_jitter, avg_jitter):
-        if self._capacitor_charge < cur_jitter:
-            self._capacitor_charge = cur_jitter
-            self._capacitor_discharge_resistance = math.pow(
-                cur_jitter / avg_jitter,
-                self._config.envelope_resistance_exponent) * self._config.envelope_resistance_coeff
-            self._capacitor_discharge_iteration = 0
-        elif self._capacitor_charge > 0:
-            self._capacitor_charge *= math.exp(
-                -self._capacitor_discharge_iteration / self._config.envelope_resistance_coeff)
-            self._capacitor_discharge_iteration += 1
-        if self._capacitor_charge < 0:
-            self._capacitor_charge = 0
-        return self._capacitor_charge
+    jitter_envelope = self._update_envelope(
+        self._smooth_jitter_window.mov_max(), self._jitter_window.mov_avg())
+    self._envelope_window.add(jitter_envelope)
+    self._peak_window.add(self._envelope_window.mov_quantile())
+
+    self._metrics.mean_jitter = round(self._jitter_window.mov_avg())
+    self._metrics.peak_jitter = round(self._peak_window.mov_max())
+    self._metrics.curr_jitter = round(jitter)
+    self._metrics.curr_envelope = round(jitter_envelope)
+
+  def _update_envelope(self, cur_jitter, avg_jitter):
+    if self._capacitor_charge < cur_jitter:
+      self._capacitor_charge = cur_jitter
+      self._capacitor_discharge_resistance = math.pow(
+        cur_jitter / avg_jitter,
+        self._config.envelope_resistance_exponent) * self._config.envelope_resistance_coeff
+      self._capacitor_discharge_iteration = 0
+    elif self._capacitor_charge > 0:
+      self._capacitor_charge *= math.exp(
+        -self._capacitor_discharge_iteration / self._config.envelope_resistance_coeff)
+      self._capacitor_discharge_iteration += 1
+    if self._capacitor_charge < 0:
+      self._capacitor_charge = 0
+    return self._capacitor_charge
---
+++ sim/mov_quantile_port.py
@@ -0,0 +1,160 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8; tab-width: 2 -*-
+
+from random import randint, random
+import numpy as np
+
+class MovQuantilePort:
+  """
+  >>> N_ITERS = 10
+  >>> N_ELEMS = 1000
+  >>> MIN_WINDOW = 1
+  >>> MAX_WINDOW = 100
+  >>> for iter in range(N_ITERS):
+  ...   win_sz = randint(MIN_WINDOW, MAX_WINDOW)
+  ...   quant = random()
+  ...   comp = MovQuantilePort(win_sz, quant)
+  ...   elems = np.zeros(N_ELEMS)
+  ...   n_elems = 0
+  ...   for n in range(N_ELEMS):
+  ...     elems[n] = random()
+  ...     n_elems += 1
+  ...     comp.add(elems[n])
+  ...     cur_win_sz = min(win_sz, n_elems)
+  ...     cur_win = np.copy(elems[n_elems - cur_win_sz:n_elems])
+  ...     cur_win.sort()
+  ...     cur_win_mid = int(np.floor((cur_win_sz - 1) * quant))
+  ...     expected = cur_win[cur_win_mid]
+  ...     actual = comp.mov_quantile()
+  ...     assert abs(expected - actual) < 0.00001
+  """
+
+  def __init__(self, win_len, quantile):
+    self._win_len = win_len
+    self._quantile = quantile
+
+    self._old_heap_root_index = 0
+    self._heap_root = 0
+    self._heap_size = 0
+    self._max_heap_index = 0
+    self._min_heap_index = 0
+    self._elem_index = 0
+    self._heap = np.zeros(win_len)
+    self._elem_index_2_heap_index = np.zeros(win_len, int)
+    self._heap_index_2_elem_index = np.zeros(win_len, int)
+
+    self._heap_root = int(quantile * float(win_len - 1))
+    self._max_heap_index = self._heap_root
+    self._min_heap_index = self._heap_root
+
+  def mov_quantile(self):
+    return self._heap[self._heap_root]
+
+  def add(self, x):
+    win_filled = (self._heap_size == self._win_len)
+    if self._heap_size < self._win_len:
+      self._heap_size += 1
+
+    if win_filled:
+      heap_index = self._elem_index_2_heap_index[self._elem_index]
+      self._heap[heap_index] = x
+      self._heapify(heap_index)
+    else:
+      k = int(self._quantile * float(self._heap_size - 1))
+      if self._elem_index == 0:
+        heap_index = self._heap_root
+        self._elem_index_2_heap_index[self._elem_index] = heap_index
+        self._heap[heap_index] = x
+        self._heap_index_2_elem_index[heap_index] = self._elem_index
+      else:
+        if self._old_heap_root_index == k:
+          self._min_heap_index += 1
+          heap_index = self._min_heap_index
+        else:
+          self._max_heap_index -= 1
+          heap_index = self._max_heap_index
+        self._elem_index_2_heap_index[self._elem_index] = heap_index
+        self._heap[heap_index] = x
+        self._heap_index_2_elem_index[heap_index] = self._elem_index
+        self._heapify(heap_index)
+        self._old_heap_root_index = k
+
+    self._elem_index = (self._elem_index + 1) % self._win_len
+
+  def _heapify(self, heap_index):
+    if heap_index < self._heap_root:
+      parent = self._heap_root - ((self._heap_root - heap_index - 1) // 2)
+      if self._heap[parent] < self._heap[heap_index]:
+        self._max_heapify_up(heap_index)
+        self._min_heapify_down(self._heap_root)
+      else:
+        self._max_heapify_down(heap_index)
+    elif self._heap_root == heap_index:
+      self._max_heapify_down(heap_index)
+      self._min_heapify_down(self._heap_root)
+    else:
+      parent = (heap_index - self._heap_root - 1) // 2 + self._heap_root
+      if self._heap[parent] > self._heap[heap_index]:
+        self._min_heapify_up(heap_index)
+        self._max_heapify_down(self._heap_root)
+      else:
+        self._min_heapify_down(heap_index)
+
+  def _min_heapify_up(self, heap_index):
+    if heap_index == self._heap_root:
+      return
+    parent = (heap_index - self._heap_root - 1) // 2 + self._heap_root
+    if self._heap[parent] > self._heap[heap_index]:
+      self._swap(heap_index, parent)
+      self._min_heapify_up(parent)
+
+  def _max_heapify_up(self, heap_index):
+    if heap_index == self._heap_root:
+      return
+    parent = self._heap_root - ((self._heap_root - heap_index - 1) // 2)
+    if self._heap[parent] < self._heap[heap_index]:
+      self._swap(heap_index, parent)
+      self._max_heapify_up(parent)
+
+  def _min_heapify_down(self, heap_index):
+    largest = heap_index
+
+    left = 2 * (heap_index - self._heap_root) + 1 + self._heap_root
+    if left <= self._min_heap_index and self._heap[left] < self._heap[largest]:
+      largest = left
+    right = 2 * (heap_index - self._heap_root) + 2 + self._heap_root
+    if right <= self._min_heap_index and self._heap[right] < self._heap[largest]:
+      largest = right
+
+    if largest != heap_index:
+      self._swap(heap_index, largest)
+      self._min_heapify_down(largest)
+
+  def _max_heapify_down(self, heap_index):
+    largest = heap_index
+
+    left = 2 * (self._heap_root - heap_index) + 1
+    if left <= (self._heap_root - self._max_heap_index) and \
+       self._heap[self._heap_root - left] > self._heap[largest]:
+      largest = self._heap_root - left
+    right = 2 * (self._heap_root - heap_index) + 2
+    if right <= (self._heap_root - self._max_heap_index) and \
+       self._heap[self._heap_root - right] > self._heap[largest]:
+      largest = self._heap_root - right
+    if largest != heap_index:
+      self._swap(heap_index, largest)
+      self._max_heapify_down(largest)
+
+  def _swap(self, index_1, index_2):
+    elem_index_1 = self._heap_index_2_elem_index[index_1]
+    elem_index_2 = self._heap_index_2_elem_index[index_2]
+
+    temp = self._heap[index_1]
+    self._heap[index_1] = self._heap[index_2]
+    self._heap[index_2] = temp
+
+    self._heap_index_2_elem_index[index_1] = elem_index_2
+    self._heap_index_2_elem_index[index_2] = elem_index_1
+
+    self._elem_index_2_heap_index[elem_index_1] = index_2
+    self._elem_index_2_heap_index[elem_index_2] = index_1
